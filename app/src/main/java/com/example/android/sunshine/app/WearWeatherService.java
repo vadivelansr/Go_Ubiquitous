0e73e4436e8bc1b4624e1202421aeeb8f1594b60
vadivelansr <vadivelan.sr@gmail.com>
2016-01-04 11:25:39 +0530

    First commit
    First commit

---
+++ app/src/main/java/com/example/android/sunshine/app/WearWeatherService.java
@@ -0,0 +1,28 @@
+package com.example.android.sunshine.app;
+
+import android.util.Log;
+
+import com.example.android.sunshine.app.sync.SunshineSyncAdapter;
+import com.google.android.gms.wearable.DataEvent;
+import com.google.android.gms.wearable.DataEventBuffer;
+import com.google.android.gms.wearable.WearableListenerService;
+
+public class WearWeatherService extends WearableListenerService {
+
+    private static final String TAG = WearWeatherService.class.getSimpleName();
+
+    private static final String WEATHER_PATH = "/weather";
+
+    @Override
+    public void onDataChanged(DataEventBuffer dataEvents) {
+        for (DataEvent dataEvent : dataEvents) {
+            if (dataEvent.getType() == DataEvent.TYPE_CHANGED) {
+                String path = dataEvent.getDataItem().getUri().getPath();
+                Log.d(TAG, path);
+                if (path.equals(WEATHER_PATH)) {
+                    SunshineSyncAdapter.syncImmediately(this);
+                }
+            }
+        }
+    }
+}
